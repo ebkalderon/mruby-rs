ca72416137742430b42bd79c642c48e43dc8cf71
Eyal Kalderon <ebkalderon@gmail.com>
2018-06-17 04:41:38 +0800

    Update copyright statement in get_mruby.sh

--- mruby-sys/vendor/get_mruby.sh
+++ mruby-sys/vendor/get_mruby.sh
@@ -1,5 +1,5 @@
-# mrusty. mruby safe bindings for Rust
-# Copyright (C) 2016  Dragoș Tiselice
+# Copyright (C) 2018 Eyal Kalderon
+# Copyright (C) 2016 Dragoș Tiselice
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
