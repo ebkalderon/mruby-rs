48ac000b5bb6a7164993d751e3dcfeda5346b9dc
Eyal Kalderon <ebkalderon@gmail.com>
2019-05-06 19:30:21 +0800

    Fix header comment in get_mruby.sh

--- mruby-sys/vendor/get_mruby.sh
+++ mruby-sys/vendor/get_mruby.sh
@@ -6,14 +6,15 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
+#
 # This script generates mruby-out.tar which is the mruby repository compiled to
 # C files.
 #
 # Dependencies:
-# * Ruby
-# * Bison
-# * compile, linker & archiver
+# * bison
+# * compiler, linker, and archiver
+# * curl
+# * ruby
 # * unzip
 
 set -Euo pipefail
